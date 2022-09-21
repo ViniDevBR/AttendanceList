c546a1dfb26318df4d21623734379da1051fed2a
ViniDevBR <vinigtr386@gmail.com>
2022-09-20 21:19:07 -0300

    :tada: Finish Home design

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,3 +1,4 @@
+import { Card } from '../../components/Card/Index'
 import './styles.css'
 
 export function Home() {
@@ -7,6 +8,11 @@ export function Home() {
       <h1>Attendace List</h1>
       <input type="text" placeholder="Digite o nome..."/>
       <button type="button">Adicionar</button>
+
+      <Card name='Vinicius' time='12:10'/>
+      <Card name='Aline' time='16:50'/>
+      <Card name='Mariana' time='13:30'/>
+      <Card name='Emanuel' time='09:20'/>
     </div>
   )
 }
